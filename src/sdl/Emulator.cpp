f1546ba0d10d30ee138527f42f9a9ce450ecf340
Nadia Holmquist Pedersen <nadia@nhp.sh>
2020-03-21 23:14:52 +0100

    Run emulator on a thread

---
+++ src/sdl/Emulator.cpp
@@ -0,0 +1,191 @@
+#include <SDL2/SDL.h>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+#include "Emulator.h"
+#include "EmuWindow.h"
+#include "PlatformConfig.h"
+
+#include "../NDS.h"
+#include "../GPU.h"
+#include "../SPU.h"
+
+SDL_GameControllerButton controller_map[] = {
+	SDL_CONTROLLER_BUTTON_B, SDL_CONTROLLER_BUTTON_A,
+	SDL_CONTROLLER_BUTTON_BACK, SDL_CONTROLLER_BUTTON_START,
+	SDL_CONTROLLER_BUTTON_DPAD_RIGHT, SDL_CONTROLLER_BUTTON_DPAD_LEFT,
+	SDL_CONTROLLER_BUTTON_DPAD_UP, SDL_CONTROLLER_BUTTON_DPAD_DOWN,
+	SDL_CONTROLLER_BUTTON_RIGHTSHOULDER, SDL_CONTROLLER_BUTTON_LEFTSHOULDER,
+	SDL_CONTROLLER_BUTTON_Y, SDL_CONTROLLER_BUTTON_X
+};
+
+Emulator::Emulator() :
+	event_queue(), event_mutex(), window(new EmuWindow()), keys(0xFFFF), touching(), emu_mutex()
+{
+	NDS::Init();
+	GPU3D::InitRenderer(false);
+	SPU::InitOutput();
+	window->set_integer_size(Config::default_scale);
+	window->set_fullscreen(Config::fullscreen);
+}
+
+Emulator::~Emulator() {
+	delete window;
+}
+
+auto Emulator::load_rom(const char* rom, const char* save, bool direct_boot) -> bool {
+	is_stopped = !NDS::LoadROM(rom, save, direct_boot);
+	return !is_stopped;
+}
+
+auto Emulator::load_firmware() -> void {
+	NDS::LoadBIOS();
+	is_stopped = false;
+}
+
+auto Emulator::run_frame() -> void {
+	if (is_stopped)
+		return;
+
+	handle_events();
+	{
+		std::unique_lock<std::mutex> lock(emu_mutex);
+		NDS::RunFrame();
+	}
+
+	auto front = GPU::FrontBuffer;
+	window->update(GPU::Framebuffer[front][0], GPU::Framebuffer[front][1]);
+}
+
+auto Emulator::handle_events() -> void {
+	std::unique_lock<std::mutex> lock(event_mutex);
+	SDL_Event e;
+
+	while (event_queue.size() > 0) {
+		e = event_queue.front();
+		event_queue.pop();
+		switch (e.type) {
+			case SDL_KEYUP:
+			case SDL_KEYDOWN: {
+				bool pressed = e.key.state == SDL_PRESSED;
+
+				if (pressed) {
+					switch (e.key.keysym.sym) {
+						case SDLK_F11:
+							window->set_fullscreen(!window->get_fullscreen());
+							break;
+						case SDLK_PLUS:
+						case SDLK_MINUS: {
+							int x, y;
+							window->get_content_size(x, y);
+							int scale = x / 256;
+							int rem = x % 256;
+							
+							if (e.key.keysym.sym == SDLK_PLUS) {
+								window->set_integer_size(scale + 1);
+							} else {
+								window->set_integer_size(scale + rem == scale ? scale - 1 : scale);
+							}
+							break;
+						}
+						case SDLK_SPACE:
+							break;
+					}
+				}
+
+				for (int i = 0; Config::keymap[i] != 0; i++) {
+					if (Config::keymap[i] == e.key.keysym.sym) {
+						if (!pressed) keys |= (1 << i);
+						else keys &= ~(1 << i);
+						break;
+					}
+				}
+
+				break;
+			}
+			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONUP:
+				if (e.button.button == SDL_BUTTON_LEFT) {
+					if (e.button.state == SDL_PRESSED) {
+						s16 x = e.button.x;
+						s16 y = e.button.y;
+
+						if (x >= 256 || y < 192 || y >= 384 || x < 0 || y < 0)
+							continue;
+
+						NDS::TouchScreen(x, y - 192);
+						NDS::PressKey(16+6);
+
+						touching = true;
+					} else {
+						NDS::ReleaseScreen();
+						NDS::ReleaseKey(16+6);
+
+						touching = false;
+					}
+				}
+				break;
+			case SDL_MOUSEMOTION:
+				if (e.motion.state == SDL_BUTTON_LMASK && touching) {
+					s16 x = e.button.x;
+					s16 y = e.button.y;
+
+					if (x >= 256 || y < 192 || y >= 384 || x < 0 || y < 0)
+						continue;
+
+					NDS::TouchScreen(x, y - 192);
+				} else if (e.motion.state != SDL_BUTTON_LMASK && touching) {
+					// Just in case
+					touching = false;
+					NDS::ReleaseScreen();
+					NDS::ReleaseKey(16+6);
+				}
+				break;
+			case SDL_CONTROLLERDEVICEADDED: {
+				u32 id = e.cdevice.which;
+				SDL_GameController* ct = SDL_GameControllerOpen(id);
+				printf("Controller connected: %s\n", SDL_GameControllerName(ct));
+				break;
+			}
+			case SDL_CONTROLLERBUTTONUP:
+			case SDL_CONTROLLERBUTTONDOWN: {
+				bool pressed = e.cbutton.state == SDL_PRESSED;
+				for (int i = 0; controller_map[i] != 0; i++) {
+					if (controller_map[i] == e.cbutton.button) {
+						if (!pressed) keys |= (1 << i);
+						else keys &= ~(1 << i);
+						break;
+					}
+				}
+
+				break;
+			}
+		}
+	}
+
+	NDS::SetKeyMask(keys);
+}
+
+auto Emulator::queue_event(SDL_Event event) -> void {
+	std::lock_guard<std::mutex> lock(event_mutex);
+	event_queue.push(event);
+}
+
+auto Emulator::read_audio(s16* data, int samples) -> int {
+	return SPU::ReadOutput(data, samples);
+}
+
+auto Emulator::get_window() -> EmuWindow* {
+	return window;
+}
+
+auto Emulator::is_running() -> bool {
+	return !is_stopped;
+}
+
+auto Emulator::stop() -> void {
+	std::lock_guard<std::mutex> lock(emu_mutex);
+	printf("stopping\n");
+	NDS::Stop();
+}
--- src/sdl/main.cpp
+++ src/sdl/main.cpp
@@ -6,29 +6,46 @@
 
 #include "EmuWindow.h"
 #include "ConfPath.h"
+#include "Emulator.h"
 
-#include "../NDS.h"
-#include "../GPU.h"
-#include "../SPI.h"
 #include "../Config.h"
-#include "../SPU.h"
 #include "PlatformConfig.h"
 
 #include "InputDialog.h"
 
-bool running = true;
-
-SDL_GameControllerButton controller_map[] = {
-	SDL_CONTROLLER_BUTTON_B, SDL_CONTROLLER_BUTTON_A,
-	SDL_CONTROLLER_BUTTON_BACK, SDL_CONTROLLER_BUTTON_START,
-	SDL_CONTROLLER_BUTTON_DPAD_RIGHT, SDL_CONTROLLER_BUTTON_DPAD_LEFT,
-	SDL_CONTROLLER_BUTTON_DPAD_UP, SDL_CONTROLLER_BUTTON_DPAD_DOWN,
-	SDL_CONTROLLER_BUTTON_RIGHTSHOULDER, SDL_CONTROLLER_BUTTON_LEFTSHOULDER,
-	SDL_CONTROLLER_BUTTON_Y, SDL_CONTROLLER_BUTTON_X
-};
+Emulator* emulator;
 
 void audio_callback(void* data, Uint8* stream, int len) {
-	SPU::ReadOutput((s16*)stream, len>>2);
+	emulator->read_audio((s16*)stream, len>>2);
+}
+
+int emu_thread(void* data) {
+	FPSmanager fps;
+	SDL_initFramerate(&fps);
+	SDL_setFramerate(&fps, 60);
+
+	SDL_AudioSpec req;
+	req.channels = 2;
+	req.format = AUDIO_S16SYS;
+	req.freq = 32768;
+	req.samples = 1024;
+	req.callback = &audio_callback;
+
+	SDL_AudioSpec out;
+	SDL_OpenAudio(&req, &out);
+
+	emulator->get_window()->show();
+
+	SDL_PauseAudio(0);
+
+	while (emulator->is_running()) {
+		emulator->run_frame();
+		SDL_framerateDelay(&fps);
+	}
+
+	printf("Emu thraed done\n");
+
+	return 0;
 }
 
 int input_config(void* a) {
@@ -58,20 +75,6 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	SDL_AudioSpec req;
-	req.channels = 2;
-	req.format = AUDIO_S16SYS;
-	req.freq = 32768;
-	req.samples = 1024;
-	req.callback = &audio_callback;
-
-	SDL_AudioSpec out;
-	SDL_OpenAudio(&req, &out);
-
-	FPSmanager fps;
-	SDL_initFramerate(&fps);
-	SDL_setFramerate(&fps, 60);
-
 	Config::Load();
 
 #ifdef JIT_ENABLED
@@ -82,162 +85,44 @@ int main(int argc, char** argv) {
 #endif
 	Config::Threaded3D = true;
 
-	auto window = new EmuWindow();
-	window->set_integer_size(Config::default_scale);
-
-	if (Config::fullscreen) {
-		window->set_fullscreen(true);
-	}
-
-	NDS::Init();
-	GPU3D::InitRenderer(false);
+	emulator = new Emulator();
 
 	if (has_rom) {
 		int arglen = strlen(argv[1]);
 		char* sav_name = (char*) malloc(arglen);
 		strcpy(sav_name, argv[1]);
 		strcpy(sav_name + (arglen - 4), ".sav");
-		NDS::LoadROM(argv[1], sav_name, false);
+		emulator->load_rom(argv[1], sav_name, false);
 	} else {
-		NDS::LoadBIOS();
+		emulator->load_firmware();
 	}
-
-	SPU::InitOutput();
-	SDL_PauseAudio(0);
 
 	SDL_Event e;
-	u32 keys = 0xFFFF;
-	bool touching;
-	bool should_delay = true;
 	bool paused = false;
 
-	window->show();
+	SDL_Thread* emu = SDL_CreateThread(emu_thread, "melonDS emulator thread", NULL);
 
-	while (running) {
-		if (window->has_focus()) {
-			while (SDL_PollEvent(&e)) {
-				switch (e.type) {
-					case SDL_QUIT:
-						running = false;
-						break;
-					case SDL_KEYUP:
-					case SDL_KEYDOWN: {
-						bool pressed = e.key.state == SDL_PRESSED;
-
-						if (pressed) {
-							switch (e.key.keysym.sym) {
-								case SDLK_F12: {
-									do_input_config();
-									break;
-								}
-								case SDLK_F11:
-									window->set_fullscreen(!window->get_fullscreen());
-									break;
-								case SDLK_PLUS:
-								case SDLK_MINUS: {
-									int x, y;
-									window->get_content_size(x, y);
-									int scale = x / 256;
-									int rem = x % 256;
-									
-									if (e.key.keysym.sym == SDLK_PLUS) {
-										window->set_integer_size(scale + 1);
-									} else {
-										window->set_integer_size(scale + rem == scale ? scale - 1 : scale);
-									}
-									break;
-								}
-								case SDLK_SPACE:
-									should_delay = !should_delay;
-									break;
-							}
-						}
-
-						for (int i = 0; Config::keymap[i] != 0; i++) {
-							if (Config::keymap[i] == e.key.keysym.sym) {
-								if (!pressed) keys |= (1 << i);
-								else keys &= ~(1 << i);
-								break;
-							}
-						}
-
-						break;
-					}
-					case SDL_MOUSEBUTTONDOWN:
-					case SDL_MOUSEBUTTONUP:
-						if (e.button.button == SDL_BUTTON_LEFT) {
-							if (e.button.state == SDL_PRESSED) {
-								s16 x = e.button.x;
-								s16 y = e.button.y;
-
-								if (x >= 256 || y < 192 || y >= 384 || x < 0 || y < 0)
-									continue;
-
-								NDS::TouchScreen(x, y - 192);
-								NDS::PressKey(16+6);
-
-								touching = true;
-							} else {
-								NDS::ReleaseScreen();
-								NDS::ReleaseKey(16+6);
-
-								touching = false;
-							}
-						}
-						break;
-					case SDL_MOUSEMOTION:
-						if (e.motion.state == SDL_BUTTON_LMASK && touching) {
-							s16 x = e.button.x;
-							s16 y = e.button.y;
-
-							if (x >= 256 || y < 192 || y >= 384 || x < 0 || y < 0)
-								continue;
-
-							NDS::TouchScreen(x, y - 192);
-						} else if (e.motion.state != SDL_BUTTON_LMASK && touching) {
-							// Just in case
-							touching = false;
-							NDS::ReleaseScreen();
-							NDS::ReleaseKey(16+6);
-						}
-						break;
-					case SDL_CONTROLLERDEVICEADDED: {
-						u32 id = e.cdevice.which;
-						SDL_GameController* ct = SDL_GameControllerOpen(id);
-						printf("Controller connected: %s\n", SDL_GameControllerName(ct));
-						break;
-					}
-					case SDL_CONTROLLERBUTTONUP:
-					case SDL_CONTROLLERBUTTONDOWN: {
-						bool pressed = e.cbutton.state == SDL_PRESSED;
-						for (int i = 0; controller_map[i] != 0; i++) {
-							if (controller_map[i] == e.cbutton.button) {
-								if (!pressed) keys |= (1 << i);
-								else keys &= ~(1 << i);
-								break;
-							}
-						}
-
-						break;
-					}
-				}
+	while (emulator->is_running()) {
+		while (SDL_PollEvent(&e)) {
+			switch (e.type) {
+				case SDL_QUIT:
+					emulator->stop();
+					break;
+				default:
+					emulator->queue_event(e);
+					break;
 			}
 		}
-
-		NDS::SetKeyMask(keys);
-		if (!paused) {
-			SDL_PauseAudio(0);
-			NDS::RunFrame();
-		} else {
-			SDL_PauseAudio(1);
-		}
-		auto front = GPU::FrontBuffer;
-		window->update(GPU::Framebuffer[front][0], GPU::Framebuffer[front][1]);
-		if (should_delay)
-			SDL_framerateDelay(&fps);
 	}
 
-	Config::Save();
 
-	return 0;
+
+	int status;
+	SDL_WaitThread(emu, &status);
+	delete emulator;
+
+	Config::Save();
+	SDL_Quit();
+
+	return status;
 }
