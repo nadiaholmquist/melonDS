1a0edc6cb0d69b63eb45a158db6df2e6fd7966f7
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-08-16 06:32:01 +0200

    vcpkg 2025.07.25

--- cmake/ConfigureVcpkg.cmake
+++ cmake/ConfigureVcpkg.cmake
@@ -9,7 +9,7 @@ if (VCPKG_ROOT STREQUAL "${_DEFAULT_VCPKG_ROOT}")
     endif()
     FetchContent_Declare(vcpkg
         GIT_REPOSITORY "https://github.com/Microsoft/vcpkg.git"
-        GIT_TAG 2ad004460f5db4d3b66f62f5799ff66c265c4b5d
+        GIT_TAG dd3097e305afa53f7b4312371f62058d2e665320
         EXCLUDE_FROM_ALL
         SOURCE_DIR "${CMAKE_SOURCE_DIR}/vcpkg")
     FetchContent_MakeAvailable(vcpkg)
